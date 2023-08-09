ae319e69f81b902a928d57729a10185d9a1a5ca2
baglaev <bglvssh@yandex.ru>
2023-08-09 17:17:47 +0300

    after 2 controllers

---
+++ controllers/user.js
@@ -0,0 +1,20 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch(() => res.status(500).send({ messag: 'На сервере произошла ошибка' }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+};
