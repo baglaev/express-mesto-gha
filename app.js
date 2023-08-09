c3a304474a9139c3ca3d81cb50004c2ad5695a9c
baglaev <bglvssh@yandex.ru>
2023-08-09 11:02:53 +0300

    schema end

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { ObjectId } = require('mongoose');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
@@ -14,6 +15,53 @@ mongoose.connect(DB_URL, {
   useUnifiedTopology: true,
 });
 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  },
+  avatar: {
+    type: String,
+    required: true
+  }
+});
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: ObjectId,
+    required: true
+  },
+  likes: [{
+    type: Array,
+    default: ''
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('card', cardSchema);
